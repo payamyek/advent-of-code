ee618bd5f7da40c084bab38b8432a38b8db8bfe7
Payam Yektamaram <payamyek@gmail.com>
2024-12-06 13:01:45 -0500

    refactor: code cleanup

--- solutions/2024/p5-b.py
+++ solutions/2024/p5-b.py
@@ -2,36 +2,6 @@ from typing import List
 from aocd import data, submit
 import re
 
-# data = """47|53
-# 97|13
-# 97|61
-# 97|47
-# 75|29
-# 61|13
-# 75|53
-# 29|13
-# 97|29
-# 53|29
-# 61|53
-# 97|53
-# 61|29
-# 47|13
-# 75|47
-# 97|75
-# 47|61
-# 75|61
-# 47|29
-# 75|13
-# 53|13
-
-# 75,47,61,53,29
-# 97,61,53,29,13
-# 75,29,13
-# 75,97,47,61,53
-# 61,13,29
-# 97,13,75,29,47
-# """
-
 ordering_rules = {}
 
 for match in re.findall(r"(\d+)\|(\d+)", data.split("\n\n")[0]):
@@ -56,8 +26,6 @@ def is_valid_production(input: List[int]) -> bool:
             return False
     return True
 
-
-print("ORDERING", ordering_rules)
 
 invalid_productions = []
 
@@ -87,18 +55,11 @@ def fix_production(input: List[int]) -> List[int]:
     return fixed_input
 
 
-print("FINAL", fix_production([97, 13, 75, 29, 47]))
-
 fixed_productions = [
     fix_production(invalid_production) for invalid_production in invalid_productions
 ]
 
-# print(fixed_productions)
 
 total = sum([production[len(production) // 2] for production in fixed_productions])
 
 submit(total)
-
-# submit(total)
-
-# 97,13,75,29,47
