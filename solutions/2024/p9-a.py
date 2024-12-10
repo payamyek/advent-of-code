b1859a1bfae1dc09c77f202a6cf162cbb408dfee
Payam Yektamaram <payamyek@gmail.com>
2024-12-10 20:45:17 +0000

    feat: p9-a solution

--- solutions/2024/p9-a.py
+++ solutions/2024/p9-a.py
@@ -17,7 +17,19 @@ def disk_map_to_block_map(disk_map: str) -> List[int]:
     return block_map
 
 
+def find_rightmost_block_index(block_map: List[int], last_block_id=-1) -> int:
+    start_index = last_block_id - 1 if last_block_id != -1 else len(block_map) - 1
+
+    for index in range(start_index, -1, -1):
+        if block_map[index] != -1:
+            return index
+    return -1
+
+
 def compute_checksum(block_map: List[int]) -> int:
+    block_map = block_map.copy()
+    rightmost_block_index = -1
+
     total = 0
 
     for index, block_id in enumerate(block_map):
@@ -25,14 +37,11 @@ def compute_checksum(block_map: List[int]) -> int:
             total += index * block_id
             continue
 
-        filled_indices = [
-            index for index, block_id in enumerate(block_map) if block_id != -1
-        ]
+        rightmost_block_index = find_rightmost_block_index(
+            block_map, rightmost_block_index
+        )
 
-        if (
-            not len(filled_indices)
-            or (rightmost_block_index := filled_indices[-1]) <= index
-        ):
+        if rightmost_block_index == -1 or rightmost_block_index <= index:
             break
 
         total += block_map[rightmost_block_index] * index
