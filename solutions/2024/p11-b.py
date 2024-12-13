886d92098671fae989380115f01e77b59267f276
Payam Yektamaram <payamyek@gmail.com>
2024-12-13 20:59:48 +0000

    feat: add p11-b

--- solutions/2024/p11-b.py
+++ solutions/2024/p11-b.py
@@ -1,29 +1,63 @@
 from typing import List
+from functools import cache
 
 from aocd import data, submit
 
 
-def blink(stones: List[int]) -> List[int]:
-    new_stones = []
+N = 75
+
+
+@cache
+def blink(stone: str) -> tuple[str, ...]:
+    result = []
+
+    if stone == "0":
+        result.append("1")
+    elif len(stone) % 2 == 0:
+        result.append(str(int(stone[: len(stone) // 2])))
+        result.append(str(int(stone[len(stone) // 2 :])))
+    else:
+        result.append(str(int(stone) * 2024))
+    return tuple(result)
+
+
+def itemize(stones: List[str]) -> dict[str, int]:
+    itemized_stones = dict()
 
     for stone in stones:
-        if stone == "0":
-            new_stones.append("1")
-        elif len(stone) % 2 == 0:
-            new_stones.append(str(int(stone[: len(stone) // 2])))
-            new_stones.append(str(int(stone[len(stone) // 2 :])))
+        if stone in itemized_stones:
+            itemized_stones[stone] += 1
         else:
-            new_stones.append(str(int(stone) * 2024))
+            itemized_stones[stone] = 1
+    return itemized_stones
 
-    return new_stones
 
+def apply_blink_to_stones(stones: dict[str, int]) -> dict[str, int]:
+    result = {}
 
-def blink_number_of_times(stones: List[int], number: int) -> List[int]:
-    result = stones[:]
-    for _ in range(number):
-        result = blink(result)
+    for key, value in stones.items():
+        blinked_stones = blink(key)
+
+        for blinked_stone in blinked_stones:
+            if blinked_stone in result:
+                result[blinked_stone] += value
+            else:
+                result[blinked_stone] = value
     return result
 
 
-result = blink_number_of_times(data.split(), 75)
-submit(len(result))
+def iterative_blink(stones: dict[str, int], number: int) -> dict[str, int]:
+    result = stones.copy()
+    for _ in range(number):
+        result = apply_blink_to_stones(result)
+    return result
+
+
+def number_of_stones(stones: dict[str, int]) -> int:
+    return sum(stones.values())
+
+
+itemized_stones = itemize(data.split())
+itemized_stones = iterative_blink(itemized_stones, N)
+
+submit(number_of_stones(itemized_stones))
