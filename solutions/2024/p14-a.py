1079e7d1a41f15f3a7157909c2c00d0b14719950
Payam Yektamaram <payamyek@gmail.com>
2024-12-17 21:55:36 +0000

    feat: p14-a skeleton

---
+++ solutions/2024/p14-a.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+import re
+from typing import List, Self
+from aocd import data, submit
+
+WIDTH = 11
+HEIGHT = 7
+
+data = """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"""
+
+
+@dataclass(frozen=True)
+class Vector:
+    x: int
+    y: int
+
+    def __add__(self, other: Self) -> Self:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __rmul__(self, other: int) -> Self:
+        if not isinstance(other, int):
+            raise Exception("Vector class only supports scalar multiplication.")
+        return Vector(self.x * other, self.y * other)
+
+
+@dataclass
+class Robot:
+    position: Vector
+    velocity: Vector
+
+    def move(self, n: int):
+        new_position = n * self.position
+
+
+def _create_robots() -> List[Robot]:
+    robots: List[Robot] = []
+
+    for row in data.splitlines():
+        match = re.search(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)", row)
+
+        position = Vector(int(match.group(1)), int(match.group(2)))
+        velocity = Vector(int(match.group(3)), int(match.group(4)))
+
+        robots.append(Robot(position, velocity))
+
+    return robots
+
+
+robots = _create_robots()
+
+print(robots)
