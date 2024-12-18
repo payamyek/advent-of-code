fd7b4a5eab12a1ffdf5554980f8104bed152222f
Payam Yektamaram <payamyek@gmail.com>
2024-12-17 23:49:33 -0500

    fix: robot move function

--- solutions/2024/p14-a.py
+++ solutions/2024/p14-a.py
@@ -5,9 +5,20 @@ from aocd import data
 
 WIDTH = 11
 HEIGHT = 7
-SECONDS = 5
+SECONDS = 100
 
-data = """p=2,4 v=2,-3
+data = """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
 """
 
 
